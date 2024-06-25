586c507ed9bfe81ab47cb53452febbea78e37a3d
ThomasKIM8365 <lefteye4me@gmail.com>
2024-06-25 11:14:31 +0900

    Initial Commit

---
+++ Book4/Book4App.swift
@@ -0,0 +1,17 @@
+//
+//  Book4App.swift
+//  Book4
+//
+//  Created by 김진규 on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Book4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
